236f05c0efaed55fd84c3d9fbfbd7f530cb642ad
Flandre <flantan233@gmail.com>
2017-01-17 18:11:12 +0800

    update

---
+++ components/App.js
@@ -0,0 +1,11 @@
+import AddTodo from "../container/AddTodo"
+import VisibleTodoList from "../container/VisibleTodoList"
+import Footer from "./Footer"
+
+const App = () => {
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+}
