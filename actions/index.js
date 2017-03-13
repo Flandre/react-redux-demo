7fa884eab3f77e855523025be5a459ee573e67c8
Flandre <flantan233@gmail.com>
2017-03-13 15:47:02 +0800

    export action type

--- actions/index.js
+++ actions/index.js
@@ -1,8 +1,12 @@
 let nextTodoId = 0;
 
+export const ADD_TODO = 'ADD_TODO';
+export const SET_VISIBILITY = 'SET_VISIBILITY';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+
 export const addTodo = (text) => {
   return {
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     id: nextTodoId++,
     text
   }
@@ -10,14 +14,14 @@ export const addTodo = (text) => {
 
 export const setVisibility = (filter) => {
   return {
-    type: 'SET_VISIBILITY',
+    type: SET_VISIBILITY,
     filter
   }
 }
 
 export const toggleTodo = (id) => {
   return {
-    type: 'TOGGLE_TODO',
+    type: TOGGLE_TODO,
     id
   }
 }
